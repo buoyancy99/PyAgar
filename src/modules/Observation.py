474d6ca9608dea2bb6154655013d7dce8e634887
Boyuan Chen <boyuanchen@berkeley.edu>
2019-07-16 22:28:08 -0700

    Update Observation.py

--- src/modules/Observation.py
+++ src/modules/Observation.py
@@ -8,10 +8,10 @@ class AgarObservation():
     def to(self, device):
         parsed_env_obs = []
         for player_obs in self.obs:
-            playercells = [torch.from_numpy(player).to(device) for player in player_obs['player']] if player_obs['player'] else None
-            foodcells = torch.from_numpy(player_obs['food']).to(device) if player_obs['food'] else None
-            viruscells = torch.from_numpy(player_obs['virus']).to(device) if player_obs['virus'] else None
-            ejectedcells = torch.from_numpy(player_obs['ejected']).to(device) if player_obs['ejected'] else None
+            playercells = [torch.from_numpy(player).to(device) for player in player_obs['player']] if player_obs['player'] is not None else None
+            foodcells = torch.from_numpy(player_obs['food']).to(device) if player_obs['food'] is not None else None
+            viruscells = torch.from_numpy(player_obs['virus']).to(device) if player_obs['virus'] is not None else None
+            ejectedcells = torch.from_numpy(player_obs['ejected']).to(device) if player_obs['ejected'] is not None else None
             parsed_env_obs.append({'player': playercells, 'food': foodcells, 'virus': viruscells, 'ejected': ejectedcells})
 
         self.tensor_obs = parsed_env_obs
